a55fedaf8ebd42aae12e57d35acae978f5d5e1be
Mykyta Zemlianyi <zemlianoyne@gmail.com>
2024-12-27 19:32:17 +0100

    add TimeTable Dto class

---
+++ src/main/java/ua/foxminded/mykyta/zemlianyi/university/dto/TimeTable.java
@@ -0,0 +1,33 @@
+package ua.foxminded.mykyta.zemlianyi.university.dto;
+
+import java.util.Set;
+
+public class TimeTable {
+    private User owner;
+    private Set<Lecture> lectures;
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public Set<Lecture> getLectures() {
+        return lectures;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public void setLectures(Set<Lecture> lectures) {
+        this.lectures = lectures;
+    }
+
+    public void addLecture(Lecture lecture) {
+        this.lectures.add(lecture);
+    }
+
+    public void removeLecture(Lecture lecture) {
+        this.lectures.remove(lecture);
+    }
+
+}
