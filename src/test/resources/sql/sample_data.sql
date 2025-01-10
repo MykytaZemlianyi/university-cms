1b1bd181f020ec3b64b11def50d52e077244e7e9
Mykyta Zemlianyi <zemlianoyne@gmail.com>
2025-01-10 15:40:26 +0100

    add sql scripts for integrational tests

---
+++ src/test/resources/sql/sample_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO university.teachers (name, surname, email, password) VALUES ("Marek", "Szepski", "mszepski@gmail.com", "szepski99");
+
+INSERT INTO university.courses (course_name, teacher_id) VALUES ("Computer Science", 1);
