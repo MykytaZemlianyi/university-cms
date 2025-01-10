e6895b33338941a0360cc3d7f6f5027ef2702413
Mykyta Zemlianyi <zemlianoyne@gmail.com>
2025-01-10 15:41:17 +0100

    add CourseDao custom queries

--- src/main/java/ua/foxminded/mykyta/zemlianyi/university/Constants.java
+++ src/main/java/ua/foxminded/mykyta/zemlianyi/university/Constants.java
@@ -6,4 +6,5 @@ public class Constants {
     }
 
     public static final String EMAIL_PATTERN_REGEX = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    public static final String FIND_COURSES_BY_TEACHER = "SELECT c FROM Course c WHERE c.teacher_id = :teacherId";
 }
--- src/main/java/ua/foxminded/mykyta/zemlianyi/university/dao/CourseDao.java
+++ src/main/java/ua/foxminded/mykyta/zemlianyi/university/dao/CourseDao.java
@@ -1,10 +1,20 @@
 package ua.foxminded.mykyta.zemlianyi.university.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import ua.foxminded.mykyta.zemlianyi.university.Constants;
 import ua.foxminded.mykyta.zemlianyi.university.dto.Course;
+import ua.foxminded.mykyta.zemlianyi.university.dto.Teacher;
+
 @Repository
 public interface CourseDao extends JpaRepository<Course, Long> {
+    @Query(Constants.FIND_COURSES_BY_TEACHER)
+    List<Course> findCoursesByTeacher(@Param("teacherId") Teacher teacher);
 
+    List<Course> findByTeacher(Teacher teacher);
 }
