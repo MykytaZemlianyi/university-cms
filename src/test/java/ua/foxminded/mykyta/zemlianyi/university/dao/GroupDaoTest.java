384c1b9f42d19e915eea19edd9452a824a82139c
Mykyta Zemlianyi <zemlianoyne@gmail.com>
2025-01-11 14:52:00 +0100

    add findByStudents in GroupDao | add tests

---
+++ src/test/java/ua/foxminded/mykyta/zemlianyi/university/dao/GroupDaoTest.java
@@ -0,0 +1,54 @@
+package ua.foxminded.mykyta.zemlianyi.university.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import ua.foxminded.mykyta.zemlianyi.university.UniversityApplication;
+import ua.foxminded.mykyta.zemlianyi.university.dto.Group;
+import ua.foxminded.mykyta.zemlianyi.university.dto.Student;
+
+@Testcontainers
+@SpringBootTest(classes = { UniversityApplication.class })
+@ActiveProfiles("test")
+@ComponentScan(basePackages = "ua.foxminded.mykyta.zemlianyi.university")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql(scripts = { "/sql/clear_tables.sql",
+        "/sql/sample_data.sql" }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+class GroupDaoTest {
+    @Autowired
+    GroupDao groupDao;
+    Student student = new Student();
+    Group group1 = new Group();
+
+    @BeforeEach
+    void setUp() {
+        student.setId(1L);
+        student.setName("Mykyta");
+        student.setSurname("Zemlianyi");
+        student.setEmail("mzemlianyi@gmail.com");
+        student.setPassword("mz2004");
+
+        group1.setId(1L);
+        group1.setName("AA-11");
+        group1.addStudent(student);
+    }
+
+    @Test
+    void findByStudent_shouldReturnCorrectGroup_whenStudentAssignedToThisGroup() {
+        Group expectedGroup = group1;
+
+        Group actualGroup = groupDao.findByStudents(student);
+
+        assertEquals(expectedGroup, actualGroup);
+    }
+
+}
--- src/test/resources/sql/sample_data.sql
+++ src/test/resources/sql/sample_data.sql
@@ -1,3 +1,7 @@
 INSERT INTO university.teachers (name, surname, email, password) VALUES ('Marek', 'Szepski', 'mszepski@gmail.com', 'szepski99');
 
 INSERT INTO university.courses (course_name, teacher_id) VALUES ('Computer Science', 1);
+
+INSERT INTO university.groups (group_name) VALUES ('AA-11');
+
+INSERT INTO university.students (name, surname, email, password, group_id) VALUES ('Mykyta', 'Zemlianyi', 'mzemlianyi@gmail.com','mz2004', 1);
