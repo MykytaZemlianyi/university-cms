985b52171123feb312a9ba6720efc4513735a3cc
Mykyta Zemlianyi <zemlianoyne@gmail.com>
2025-01-11 13:15:04 +0100

    make Admin managed class

--- src/main/java/ua/foxminded/mykyta/zemlianyi/university/dto/Admin.java
+++ src/main/java/ua/foxminded/mykyta/zemlianyi/university/dto/Admin.java
@@ -1,5 +1,10 @@
 package ua.foxminded.mykyta.zemlianyi.university.dto;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "admins", schema = "university")
 public class Admin extends User {
-    
+
 }
