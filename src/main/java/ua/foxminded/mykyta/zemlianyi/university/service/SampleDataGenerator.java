2eeed949f80c3dce3a1bdf06bb0246c64f448c49
Mykyta Zemlianyi <zemlianoyne@gmail.com>
2025-02-28 23:20:42 +0100

    finish logging logic for Spring Security

---
+++ src/main/java/ua/foxminded/mykyta/zemlianyi/university/service/SampleDataGenerator.java
@@ -0,0 +1,56 @@
+package ua.foxminded.mykyta.zemlianyi.university.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
+import ua.foxminded.mykyta.zemlianyi.university.dao.AdminDao;
+import ua.foxminded.mykyta.zemlianyi.university.dao.StudentDao;
+import ua.foxminded.mykyta.zemlianyi.university.dao.TeacherDao;
+
+@Service
+public class SampleDataGenerator {
+    private AdminDao adminDao;
+    private TeacherDao teacherDao;
+    private StudentDao studentDao;
+    private PasswordEncoder passwordEncoder;
+
+    public SampleDataGenerator(AdminDao adminDao, TeacherDao teacherDao, StudentDao studentDao,
+            PasswordEncoder passwordEncoder) {
+        this.adminDao = adminDao;
+        this.teacherDao = teacherDao;
+        this.studentDao = studentDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostConstruct
+    public void encryptPasswords() {
+        encryptAdminsPasswords();
+        encryptTeachersPasswords();
+        encryptStudentsPasswords();
+    }
+
+    private void encryptAdminsPasswords() {
+        adminDao.findAll().stream().forEach(admin -> {
+            String rawPassword = admin.getPassword();
+            admin.setPassword(passwordEncoder.encode(rawPassword));
+            adminDao.save(admin);
+        });
+    }
+
+    private void encryptTeachersPasswords() {
+        teacherDao.findAll().stream().forEach(teacher -> {
+            String rawPassword = teacher.getPassword();
+            teacher.setPassword(passwordEncoder.encode(rawPassword));
+            teacherDao.save(teacher);
+        });
+    }
+
+    private void encryptStudentsPasswords() {
+        studentDao.findAll().stream().forEach(student -> {
+            String rawPassword = student.getPassword();
+            student.setPassword(passwordEncoder.encode(rawPassword));
+            studentDao.save(student);
+        });
+    }
+}
--- src/main/resources/db/migration/sample_data/V5__sample_data.sql
+++ src/main/resources/db/migration/sample_data/V5__sample_data.sql
@@ -1,9 +1,9 @@
 
 -- Insert administrators
 INSERT INTO university.admins (name, surname, email, password) VALUES
-('Mykyta', 'Zemlianyi', 'zemlianoyne@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('John', 'Smith', 'john.smith@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Emma', 'Johnson', 'emma.johnson@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG');
+('Mykyta', 'Zemlianyi', 'zemlianoyne@gmail.com', '12345'),
+('John', 'Smith', 'john.smith@gmail.com', '12345'),
+('Emma', 'Johnson', 'emma.johnson@gmail.com', '12345');
 
 -- Insert groups
 INSERT INTO university.groups (group_name) VALUES
@@ -12,11 +12,11 @@ INSERT INTO university.groups (group_name) VALUES
 
 -- Insert teachers
 INSERT INTO university.teachers (name, surname, email, password) VALUES
-('Michael', 'Brown', 'michael.brown@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('David', 'Wilson', 'david.wilson@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Sarah', 'Moore', 'sarah.moore@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('James', 'Taylor', 'james.taylor@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Olivia', 'Anderson', 'olivia.anderson@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG');
+('Michael', 'Brown', 'michael.brown@gmail.com', '12345'),
+('David', 'Wilson', 'david.wilson@gmail.com', '12345'),
+('Sarah', 'Moore', 'sarah.moore@gmail.com', '12345'),
+('James', 'Taylor', 'james.taylor@gmail.com', '12345'),
+('Olivia', 'Anderson', 'olivia.anderson@gmail.com', '12345');
 
 -- Insert courses
 INSERT INTO university.courses (course_name) VALUES
@@ -28,21 +28,21 @@ INSERT INTO university.courses (course_name) VALUES
 
 -- Insert students
 INSERT INTO university.students (name, surname, email, password) VALUES
-('William', 'Hall', 'william.hall@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Alexander', 'Allen', 'alexander.allen@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Ethan', 'Young', 'ethan.young@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Daniel', 'King', 'daniel.king@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Benjamin', 'Wright', 'benjamin.wright@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Matthew', 'Scott', 'matthew.scott@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Lucas', 'Adams', 'lucas.adams@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Henry', 'Baker', 'henry.baker@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Jack', 'Harris', 'jack.harris@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Logan', 'Nelson', 'logan.nelson@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Mason', 'Carter', 'mason.carter@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Jacob', 'Mitchell', 'jacob.mitchell@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Noah', 'Perez', 'noah.perez@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Owen', 'Roberts', 'owen.roberts@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG'),
-('Elijah', 'Turner', 'elijah.turner@gmail.com', '$2a$10$S7kE9HXuqvvXsGkG2dElWeHbaJqz9ok2e3vhcfbTEtkAzI5A0V6qG');
+('William', 'Hall', 'william.hall@gmail.com', '12345'),
+('Alexander', 'Allen', 'alexander.allen@gmail.com', '12345'),
+('Ethan', 'Young', 'ethan.young@gmail.com', '12345'),
+('Daniel', 'King', 'daniel.king@gmail.com', '12345'),
+('Benjamin', 'Wright', 'benjamin.wright@gmail.com', '12345'),
+('Matthew', 'Scott', 'matthew.scott@gmail.com', '12345'),
+('Lucas', 'Adams', 'lucas.adams@gmail.com', '12345'),
+('Henry', 'Baker', 'henry.baker@gmail.com', '12345'),
+('Jack', 'Harris', 'jack.harris@gmail.com', '12345'),
+('Logan', 'Nelson', 'logan.nelson@gmail.com', '12345'),
+('Mason', 'Carter', 'mason.carter@gmail.com', '12345'),
+('Jacob', 'Mitchell', 'jacob.mitchell@gmail.com', '12345'),
+('Noah', 'Perez', 'noah.perez@gmail.com', '12345'),
+('Owen', 'Roberts', 'owen.roberts@gmail.com', '12345'),
+('Elijah', 'Turner', 'elijah.turner@gmail.com', '12345');
 
 -- Insert rooms
 INSERT INTO university.rooms (room_number) VALUES
@@ -53,15 +53,15 @@ INSERT INTO university.rooms (room_number) VALUES
 
 -- Insert lectures
 INSERT INTO university.lectures (lecture_type,lecture_time_start, lecture_time_end) VALUES
-('Lec', '2025-03-01 08:00:00', '2025-03-01 09:30:00'),
-('Lab', '2025-03-01 10:00:00', '2025-03-01 11:30:00'),
-('Sem', '2025-03-02 08:00:00', '2025-03-02 09:30:00'),
-('Lec', '2025-03-02 10:00:00', '2025-03-02 11:30:00'),
-('Lab', '2025-03-03 08:00:00', '2025-03-03 09:30:00'),
-('Sem', '2025-03-03 10:00:00', '2025-03-03 11:30:00'),
-('Lec', '2025-03-04 08:00:00', '2025-03-04 09:30:00'),
-('Lab', '2025-03-04 10:00:00', '2025-03-04 11:30:00'),
-('Lec', '2025-03-05 08:00:00', '2025-03-04 09:30:00'),
-('Lab', '2025-03-05 10:00:00', '2025-03-04 11:30:00'),
-('Lec', '2025-03-06 08:00:00', '2025-03-04 09:30:00'),
-('Lab', '2025-03-06 10:00:00', '2025-03-04 11:30:00');
+('LECTURE', '2025-03-01 08:00:00', '2025-03-01 09:30:00'),
+('LABORATORIUM', '2025-03-01 10:00:00', '2025-03-01 11:30:00'),
+('SEMINAR', '2025-03-02 08:00:00', '2025-03-02 09:30:00'),
+('LECTURE', '2025-03-02 10:00:00', '2025-03-02 11:30:00'),
+('LABORATORIUM', '2025-03-03 08:00:00', '2025-03-03 09:30:00'),
+('SEMINAR', '2025-03-03 10:00:00', '2025-03-03 11:30:00'),
+('LECTURE', '2025-03-04 08:00:00', '2025-03-04 09:30:00'),
+('LABORATORIUM', '2025-03-04 10:00:00', '2025-03-04 11:30:00'),
+('LECTURE', '2025-03-05 08:00:00', '2025-03-04 09:30:00'),
+('SEMINAR', '2025-03-05 10:00:00', '2025-03-04 11:30:00'),
+('LECTURE', '2025-03-06 08:00:00', '2025-03-04 09:30:00'),
+('LABORATORIUM', '2025-03-06 10:00:00', '2025-03-04 11:30:00');
