27e7f0049a8f5e47613ba1f363dc9d879939a8fe
Mykyta Zemlianyi <zemlianoyne@gmail.com>
2025-03-07 11:25:30 +0100

    changed how CrudRepository inherited in UserDao and it's childs

--- src/main/java/ua/foxminded/mykyta/zemlianyi/university/dao/AdminDao.java
+++ src/main/java/ua/foxminded/mykyta/zemlianyi/university/dao/AdminDao.java
@@ -1,11 +1,10 @@
 package ua.foxminded.mykyta.zemlianyi.university.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ua.foxminded.mykyta.zemlianyi.university.dto.Admin;
 
 @Repository
-public interface AdminDao extends JpaRepository<Admin, Long>, UserDao {
+public interface AdminDao extends UserDao<Admin> {
 
 }
--- src/main/java/ua/foxminded/mykyta/zemlianyi/university/dao/UserDao.java
+++ src/main/java/ua/foxminded/mykyta/zemlianyi/university/dao/UserDao.java
@@ -2,10 +2,16 @@ package ua.foxminded.mykyta.zemlianyi.university.dao;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+
 import ua.foxminded.mykyta.zemlianyi.university.dto.User;
 
-public interface UserDao<T extends User> {
+public interface UserDao<T extends User> extends CrudRepository<T, Long> {
     boolean existsByEmail(String email);
 
     Optional<T> findByEmail(String email);
+
+    Page<T> findAll(Pageable page);
 }
