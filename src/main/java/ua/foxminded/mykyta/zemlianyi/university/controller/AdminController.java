336615d720d2ea65e2cd7c8d0568e60d19fc4dfc
Mykyta Zemlianyi <zemlianoyne@gmail.com>
2025-03-12 22:08:18 +0100

    refactored lectures and rooms to use corresponding table fragments

--- src/main/java/ua/foxminded/mykyta/zemlianyi/university/controller/AdminController.java
+++ src/main/java/ua/foxminded/mykyta/zemlianyi/university/controller/AdminController.java
@@ -1,8 +1,5 @@
 package ua.foxminded.mykyta.zemlianyi.university.controller;
 
-import java.util.LinkedHashMap;
-import java.util.function.Function;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -11,21 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import ua.foxminded.mykyta.zemlianyi.university.Constants;
-import ua.foxminded.mykyta.zemlianyi.university.dto.Admin;
-import ua.foxminded.mykyta.zemlianyi.university.dto.Course;
-import ua.foxminded.mykyta.zemlianyi.university.dto.Group;
-import ua.foxminded.mykyta.zemlianyi.university.dto.Lecture;
-import ua.foxminded.mykyta.zemlianyi.university.dto.Room;
-import ua.foxminded.mykyta.zemlianyi.university.dto.Student;
-import ua.foxminded.mykyta.zemlianyi.university.dto.Teacher;
-import ua.foxminded.mykyta.zemlianyi.university.service.AdminService;
-import ua.foxminded.mykyta.zemlianyi.university.service.CourseService;
-import ua.foxminded.mykyta.zemlianyi.university.service.GroupService;
-import ua.foxminded.mykyta.zemlianyi.university.service.LectureService;
-import ua.foxminded.mykyta.zemlianyi.university.service.RoomService;
-import ua.foxminded.mykyta.zemlianyi.university.service.StudentService;
-import ua.foxminded.mykyta.zemlianyi.university.service.TeacherService;
+import ua.foxminded.mykyta.zemlianyi.university.dto.*;
+import ua.foxminded.mykyta.zemlianyi.university.service.*;
 
 @Controller
 public class AdminController {
@@ -124,39 +108,7 @@ public class AdminController {
         Pageable pageable = PageRequest.of(page, size);
         Page<Lecture> lectures = lectureService.findAll(pageable);
 
-        LinkedHashMap<String, Function<Lecture, Object>> columnData = new LinkedHashMap<>();
-
-        columnData.put("ID", Lecture::getId);
-        columnData.put("Date", lecture -> lecture.getTimeStart().format(Constants.DATE_FORMATTER));
-        columnData.put("Begins", lecture -> lecture.getTimeStart().format(Constants.TIME_FORMATTER));
-        columnData.put("Ends", lecture -> lecture.getTimeEnd().format(Constants.TIME_FORMATTER));
-        columnData.put("Room", lecture -> lecture.getRoom().getNumber());
-        columnData.put("Type", Lecture::getLectureType);
-        columnData.put("Course", lecture -> lecture.getCourse().getName());
-        columnData.put("Teacher", lecture -> {
-            Course course = lecture.getCourse();
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append(course.getTeacher().getName());
-            stringBuilder.append(Constants.SPACE);
-            stringBuilder.append(course.getTeacher().getSurname());
-            return stringBuilder.toString();
-        });
-
-        columnData.put("Groups", lecture -> {
-            StringBuilder stringBuilder = new StringBuilder();
-            lecture.getCourse().getGroups().stream().forEach(group -> {
-                if (!stringBuilder.isEmpty()) {
-                    stringBuilder.append(Constants.SPACE);
-                    stringBuilder.append(Constants.PIPE);
-                    stringBuilder.append(Constants.SPACE);
-                }
-                stringBuilder.append(group.getName());
-            });
-            return stringBuilder.toString();
-        });
-
         model.addAttribute("lectures", lectures);
-        model.addAttribute("columns", columnData);
         model.addAttribute("currentPage", page);
         model.addAttribute("totalPages", lectures.hasContent() ? lectures.getTotalPages() : 1);
 
@@ -170,12 +122,7 @@ public class AdminController {
         Pageable pageable = PageRequest.of(page, size);
         Page<Room> rooms = roomService.findAll(pageable);
 
-        LinkedHashMap<String, Function<Room, Object>> columnData = new LinkedHashMap<>();
-        columnData.put("ID", Room::getId);
-        columnData.put("Room number", Room::getNumber);
-
         model.addAttribute("rooms", rooms);
-        model.addAttribute("columns", columnData);
         model.addAttribute("currentPage", page);
         model.addAttribute("totalPages", rooms.hasContent() ? rooms.getTotalPages() : 1);
 
