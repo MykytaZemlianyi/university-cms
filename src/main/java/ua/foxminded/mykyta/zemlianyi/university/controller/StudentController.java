5fcd316c94e1d917cfa8ef4420ff65be08670e00
Mykyta Zemlianyi <zemlianoyne@gmail.com>
2025-04-25 16:34:25 +0200

    add edit-view and controller mapping for edit-student feature

--- src/main/java/ua/foxminded/mykyta/zemlianyi/university/controller/StudentController.java
+++ src/main/java/ua/foxminded/mykyta/zemlianyi/university/controller/StudentController.java
@@ -1,6 +1,7 @@
 package ua.foxminded.mykyta.zemlianyi.university.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -8,13 +9,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.validation.Valid;
+import ua.foxminded.mykyta.zemlianyi.university.Constants;
 import ua.foxminded.mykyta.zemlianyi.university.dto.Group;
 import ua.foxminded.mykyta.zemlianyi.university.dto.Student;
 import ua.foxminded.mykyta.zemlianyi.university.service.GroupService;
@@ -53,7 +57,7 @@ public class StudentController {
     }
 
     @PostMapping("/admin/add-student")
-    public String createAdmin(@RequestParam(required = false) Long groupId, @Valid @ModelAttribute Student student,
+    public String createStudent(@RequestParam(required = false) Long groupId, @Valid @ModelAttribute Student student,
             BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         if (bindingResult.hasErrors()) {
@@ -71,4 +75,53 @@ public class StudentController {
         }
     }
 
+    @GetMapping("/admin/edit-student/{id}")
+    public String showEditAdminForm(@PathVariable Long id, Model model, RedirectAttributes redirectAttributes) {
+        Optional<Student> student = studentService.findById(id);
+        List<Group> allGroups = groupService.findAll();
+        if (student.isPresent()) {
+            model.addAttribute("student", student.get());
+            model.addAttribute("groups", allGroups);
+            return "edit-student";
+        } else {
+            redirectAttributes.addFlashAttribute("errorMessage", "Error: " + Constants.USER_NOT_FOUND_ERROR);
+            return "redirect:/admin/students";
+        }
+    }
+
+    @PostMapping("/admin/edit-student/{id}")
+    public String updateAdmin(@PathVariable Long id, @RequestParam(required = false) Long groupId,
+            @Valid @ModelAttribute("student") Student updatedStudent, BindingResult bindingResult,
+            RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            return "edit-admin";
+        }
+
+        try {
+            studentService.resolveGroupFieldById(updatedStudent, groupId);
+            studentService.update(updatedStudent);
+            redirectAttributes.addFlashAttribute("successMessage", "Student updated successfully!");
+            return "redirect:/admin/students";
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Error: " + e.getMessage());
+            return "redirect:/admin/students";
+        }
+    }
+
+    @DeleteMapping("/admin/delete-student/{id}")
+    public String deleteAdmin(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        try {
+            Optional<Student> student = studentService.findById(id);
+            if (student.isPresent()) {
+                studentService.delete(student.get());
+                redirectAttributes.addFlashAttribute("successMessage", "Student deleted successfully!");
+            } else {
+                redirectAttributes.addFlashAttribute("errorMessage", "Error: Student does not exists");
+            }
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Error: " + e.getMessage());
+        }
+        return "redirect:/admin/students";
+    }
 }
--- src/main/java/ua/foxminded/mykyta/zemlianyi/university/service/StudentServiceImpl.java
+++ src/main/java/ua/foxminded/mykyta/zemlianyi/university/service/StudentServiceImpl.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import ua.foxminded.mykyta.zemlianyi.university.Constants;
 import ua.foxminded.mykyta.zemlianyi.university.dao.GroupDao;
 import ua.foxminded.mykyta.zemlianyi.university.dao.StudentDao;
 import ua.foxminded.mykyta.zemlianyi.university.dto.Group;
@@ -23,9 +24,26 @@ public class StudentServiceImpl extends UserServiceImpl<Student> implements Stud
     }
 
     @Override
-    protected Student mergeWithExisting(Student user) {
-        // TODO Auto-generated method stub
-        return user;
+    protected Student mergeWithExisting(Student newStudent) {
+        ObjectChecker.checkNull(newStudent);
+        Optional<Student> existingStudentOpt = dao.findById(newStudent.getId());
+
+        if (existingStudentOpt.isPresent()) {
+            Student existingStudent = existingStudentOpt.get();
+            Student mergedStudent = new Student();
+
+            mergedStudent.setId(existingStudent.getId());
+            mergedStudent.setName(chooseString(newStudent.getName(), existingStudent.getName()));
+            mergedStudent.setSurname(chooseString(newStudent.getSurname(), existingStudent.getSurname()));
+            mergedStudent.setEmail(chooseString(newStudent.getEmail(), existingStudent.getEmail()));
+            mergedStudent.setGroup(newStudent.getGroup());
+            mergedStudent.setPassword(choosePassword(newStudent.getPassword(), existingStudent.getPassword()));
+
+            return mergedStudent;
+
+        } else {
+            throw new IllegalArgumentException(Constants.USER_NOT_FOUND_ERROR);
+        }
     }
 
     @Override
@@ -37,6 +55,22 @@ public class StudentServiceImpl extends UserServiceImpl<Student> implements Stud
             }
         }
         return student;
+    }
+
+    private String chooseString(String newString, String existingString) {
+        if (newString == null || newString.isBlank() || newString.equals(existingString)) {
+            return existingString;
+        } else {
+            return newString;
+        }
+    }
+
+    private String choosePassword(String newPassword, String existingPassword) {
+        if (newPassword == null || newPassword.isBlank() || newPassword.equals(existingPassword)) {
+            return existingPassword;
+        } else {
+            return passwordEncoder.encode(newPassword);
+        }
     }
 
 }
