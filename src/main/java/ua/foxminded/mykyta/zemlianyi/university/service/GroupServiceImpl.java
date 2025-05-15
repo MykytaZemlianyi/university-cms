4f72424700d7ae599bf7e95c31c260387eda9994
Mykyta Zemlianyi <zemlianoyne@gmail.com>
2025-05-15 19:35:01 +0200

    removed unused imports

--- src/main/java/ua/foxminded/mykyta/zemlianyi/university/service/GroupServiceImpl.java
+++ src/main/java/ua/foxminded/mykyta/zemlianyi/university/service/GroupServiceImpl.java
@@ -9,7 +9,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
